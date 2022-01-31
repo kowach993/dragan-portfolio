d47a528f1141225204503fe43cd91c67bcd61b6d
Dragan <ecomtechsol@gmail.com>
2022-01-31 22:03:03 +0100

    frist commit

---
+++ js/chunk-2d22d746.202367f6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,t,e){"use strict";e.r(t);var s=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",{staticClass:"about"},[n._v(" s ")])},u=[],a=e("2877"),c={},l=Object(a["a"])(c,s,u,!1,null,null,null);t["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.202367f6.js.map
